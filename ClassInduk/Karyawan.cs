275d3fbc5594ae01eb567c31784341fc9849b47e
RaisJaya <61975603+RaisJaya@users.noreply.github.com>
2020-06-29 12:28:08 +0700

    Update Karyawan.cs

--- ClassInduk/Karyawan.cs
+++ ClassInduk/Karyawan.cs
@@ -8,8 +8,9 @@ namespace TugasLab9.ClassInduk
 {
     public abstract class Karyawan
     {
-        public string Nama { get; set; }
+       public string Nama { get; set; }
         public string NIK { get; set; }
+        public string Jenis { get; set; }
         public abstract double Gaji();
     }
 }
